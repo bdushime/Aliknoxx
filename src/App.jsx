e4d45f867516c295747f53a7abd06414a27b3236
bdushime <bdushime47@gmail.com>
2026-02-27 12:32:47 +0200

    fixing tracks

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Albums from './pages/Albums';
-import AlbumDetails from './pages/AlbumDetails';
+import AlbumDetails from './pages/AlbumDetails'; // or components/AlbumDetails depending on your folder structure
 import Tracks from './pages/Tracks';
+import TrackDetails from './pages/TrackDetails'; // Added!
 import Artists from './pages/Artists';
 import ArtistDetails from './pages/ArtistDetails';
 import News from './pages/News';
+import NewsDetails from './pages/NewsDetails';   // Added!
 import Contact from './pages/Contact';
 import About from './pages/About';
 import ScrollToTop from './components/ScrollToTop';
 
-// Simple ScrollToTop component to reset scroll on route change
 function ScrollToTopWrapper() {
   return <ScrollToTop />;
 }
@@ -19,15 +20,22 @@ function ScrollToTopWrapper() {
 function App() {
   return (
     <Layout>
-       <ScrollToTopWrapper /> { /* Will implement ScrollToTop component or inline logic */ }
+       <ScrollToTopWrapper />
        <Routes>
          <Route path="/" element={<Home />} />
+         
          <Route path="/albums" element={<Albums />} />
          <Route path="/albums/:id" element={<AlbumDetails />} />
+         
          <Route path="/tracks" element={<Tracks />} />
-         <Route path="/artists" element={<Artists />} />
-         <Route path="/artists/:id" element={<ArtistDetails />} />
+         <Route path="/tracks/:id" element={<TrackDetails />} /> 
+         
+         {/* <Route path="/artists" element={<Artists />} />
+         <Route path="/artists/:id" element={<ArtistDetails />} /> */}
+         
          <Route path="/news" element={<News />} />
+         <Route path="/news/:id" element={<NewsDetails />} /> 
+         
          <Route path="/contact" element={<Contact />} />
          <Route path="/about" element={<About />} />
        </Routes>
@@ -35,4 +43,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
